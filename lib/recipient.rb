37cd7f388c3a9d0aa6333e9236bc9c57ed038b58
Sibi <sibi@psibi.in>
2014-09-13 00:12:49 +0530

    Initial working code.

---
+++ lib/recipient.rb
@@ -0,0 +1,49 @@
+# The file represents Recipient
+# (http://stickystamp.com/docs#recipient-desc) of StickStamp API.
+#
+# Author::    Sibi  (mailto:sibi@psibi.in)
+# Copyright:: Copyright (c) 2014 Sibi
+# License::   MIT
+
+require 'httpclient'
+require 'json'
+
+class Recipient
+
+  # Initialize the class with recipient details
+  def initialize(name, email, address1, address2, city, state,
+                 country, pincode, contact_number, id=nil)
+    @name = name
+    @email = email
+    @address1 = address1
+    @address2 = address2
+    @city = city
+    @state = state
+    @country = country
+    @pincode = pincode
+    @contact_number = contact_number
+    @id = id
+  end
+
+  def serialize_to_hash
+    {"id" => @id, "name" => @name, "email" => @email,
+     "address1" => @address1, "address2" => @address2, "city" => @city,
+     "state" => @state, "country" => @country, "pincode" => @pincode,
+      "contact_number" => @contact_number }.reject {|k,v| v == nil}
+  end
+
+  def self.serialize_to_Recipient(hash_data)
+    Recipient.new(hash_data["name"], hash_data["email"], hash_data["address1"],
+                  hash_data["address2"], hash_data["city"], hash_data["state"],
+                  hash_data["country"], hash_data["pincode"], hash_data["contact_number"],
+                  hash_data["id"])
+  end
+    
+  # Show in pretty form in irb
+  # def to_s
+  #   @name + " " + @email
+  # end
+
+end
+
+    
---
+++ lib/stickystamp.rb
@@ -0,0 +1,65 @@
+require 'net/http'
+require 'net/https'
+require 'json'
+require 'uri'
+require './recipient'
+
+class StickStamp
+
+  def initialize(apikey)
+    @apikey = apikey
+    @rootURL = "https://api.stickystamp.com"
+  end
+
+  def post(path, json_string)
+    uri = URI(@rootURL + path)
+    req = Net::HTTP::Post.new uri.path
+    req.basic_auth @apikey, ""
+    req.body = json_string.to_s
+    res = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.ssl_version = :SSLv3
+      http.request req
+    end
+    puts res.body
+  end
+
+  def get(path, json_string)
+    uri = URI(@rootURL + path)
+    req = Net::HTTP::Get.new uri.path
+    req.basic_auth @apikey, ""
+    req.body = json_string
+    res = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.ssl_version = :SSLv3
+      http.request req
+    end
+    res.body
+  end
+    
+
+  def createRecipient(recipient)
+    post("/v1/recipients", recipient.serialize_to_hash.to_json)
+  end
+
+  def getRecipients(recipient)
+    response = get("/v1/recipients", recipient.serialize_to_hash.to_json)
+    res_json = JSON.parse response
+    if res_json["status"] != "success"
+      raise "StickyStamp: " + res_json["error"]
+    else
+      recipients = []
+      res_json["recipients"].each do |value| 
+        recipients << Recipient.serialize_to_Recipient(value) 
+      end
+      recipients
+    end
+  end
+
+end
+  
+# r = Recipient.new("Sibi","sibi@psibi.in","add1","add2","Chennai","TN","IN","666666","777777777")
+# a = StickStamp.new("b79bdeaa19f147afbbe2d7ef9dee9be2")
+# a.createRecipient(r)
+# RestClient.post "http://api.stickystamp.com/v1/recipients", r.serialize_to_hash.to_json, :content_type => :json, :accept => :json
+
