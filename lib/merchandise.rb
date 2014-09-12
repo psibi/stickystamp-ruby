e5dadc34850b566cd1d004287283a91954e28211
Sibi <sibi@psibi.in>
2014-09-13 03:03:20 +0530

    Merchandise class

---
+++ lib/merchandise.rb
@@ -0,0 +1,26 @@
+class Merchandise
+    
+    def initialize(id, name, category, color=nil,
+                   tshirt_type=nil, dimension=nil,
+                   dimension_unit=nil)
+      @id = id
+      @name = name
+      @category = category
+      @color = color
+      @tshirt_type = tshirt_type
+      @dimension = dimension
+      @dimension_unit = dimension_unit
+    end
+
+    def self.serialize_to_Merchandise(hash_data)
+      Merchandise.new(hash_data["id"], hash_data["name"],
+                      hash_data["category"], hash_data["color"],
+                      hash_data["tshirt_type"], hash_data["dimension"],
+                      hash_data["dimension_unit"])
+    end
+
+    def to_s
+      @name + " :" + @category
+    end
+
+end
