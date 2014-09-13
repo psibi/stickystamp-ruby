4ccff1c3b83723d6d1a9ddeb3a16f22dcdb66dee
Sibi <sibi@psibi.in>
2014-09-13 12:31:14 +0530

    Added missing fields.

--- lib/grantform.rb
+++ lib/grantform.rb
@@ -4,10 +4,13 @@ class GrantForm
   :expires_on, :mailed_to, :from_title, :choices, :shipment,
   :revoked, :expired
 
-  def initialize(id=nil, token=nil, url=nil, is_valid=nil,
+  def initialize(contents=nil, recipient=nil,
+                 id=nil, token=nil, url=nil, is_valid=nil,
                  converted=nil, expires_on=nil, mailed_to=nil,
                  from_title=nil, choices=nil, shipment=nil, revoked=nil,
                  expired=nil)
+    @contents = contents
+    @recipient = recipient.serialize_to_hash
     @id = id
     @token = token
     @url = url
@@ -22,15 +25,27 @@ class GrantForm
     @expired = expired
   end
 
+  def serialize_to_hash
+    {
+      "contents" => @contents, "recipient" => @recipient, "id" => @id,
+      "token" => @token, "url" => @url, "is_valid" => @is_valid,
+      "converted" => @converted, "expires_on" => @expires_on,
+      "mailed_to" => @mailed_to, "from_title" => @from_title,
+      "choices" => @choices, "shipment" => @shipment, "revoked" => @revoked,
+      "expired" => @expired
+    }.reject {|k,v| v == nil}
+  end
+
   def self.serialize_to_GrantForm(hash_data)
-    GrantForm.new(hash_data["id"], hash_data["token"], hash_data["url"],
+    GrantForm.new(hash_data["contents"], Recipient.serialize_to_Recipient(hash_data["recipient"]), 
+                  hash_data["id"], hash_data["token"], hash_data["url"],
                   hash_data["is_valid"], hash_data["converted"], hash_data["expires_on"],
                   hash_data["mailed_to"], hash_data["from_title"], hash_data["choices"],
                   hash_data["shipment"], hash_data["revoked"], hash_data["expired"])
   end
 
   def to_s
-    @id
+    @recipient["name"]
   end
 
 end
